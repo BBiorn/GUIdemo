3bc461371c129e105af47efa534a8d3ef05badfb
BBiorn <35784219+BBiorn@users.noreply.github.com>
2018-02-14 20:25:09 -0600

    Added some components to test the JFrame

--- GUIproject/src/GUIdemo.java
+++ GUIproject/src/GUIdemo.java
@@ -1,8 +1,20 @@
 
+import java.awt.BorderLayout;
+import javax.swing.*;
+
 public class GUIdemo 
 {
    public static void main(String[] Args)
    {
+	   JFrame frame = new JFrame("GUI Demo");
+	   frame.setSize(600, 450);
 	   
+	   JPanel panel = new JPanel();
+	   panel.setLayout(new BorderLayout());
+	   JTextField tf = new JTextField("This is a test of java swing");
+	   panel.add(tf, BorderLayout.NORTH);
+	   
+	   frame.add(panel);
+	   frame.setVisible(true);	   
    }
 }
